135869de5e78cb00f216f9016e4ecd5481af92e3
Sally Pang <sally.pang@dynamic-leap.com>
2016-04-18 09:48:47 -0700

    Map

--- NameThatStreet/VocabTableViewController.m
+++ NameThatStreet/VocabTableViewController.m
@@ -94,4 +94,15 @@
     return cell;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.vocabs removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+    }
+}
+
 @end
